ff71e7ab4e69bbf9244b4cf912015bc236976e7a
Int3rsl4vist <146725090+Int3rsl4vist@users.noreply.github.com>
2025-10-24 09:53:19 +0200

    Add supabaseAdmin client for server-side access

--- supabaseClient.js
+++ supabaseClient.js
@@ -6,3 +6,8 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   { auth: { persistSession: false } }
 );
+export const supabaseAdmin = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
+  { auth: { persistSession: false } }
+);
